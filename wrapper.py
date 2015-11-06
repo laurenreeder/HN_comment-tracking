72600df6b1c0fc83b304bf215501f98c8a75ef87
Lauren Reeder <laurenmreeder@gmail.com>
2015-11-05 22:49:12 -0500

    basic plotting working, need to fix axis labelling

--- wrapper.py
+++ wrapper.py
@@ -1,26 +1,39 @@
 import psycopg2
 import matplotlib.pyplot as plt
+import pandas as pd
 import sys
 
-# Try to connect
+
+# matplotlib.style.use('ggplot')
 
 try:
     conn = psycopg2.connect("dbname='hacker_news' user='postgres' host='localhost' password='1234'")
 except:
-    print "I am unable to connect to the database."
+    print "Unable to connect to the database."
 
 cur = conn.cursor()
 
-input_var = raw_input("Enter search term: ")
-input_var = "'%"+input_var+"%'"   # format SQL string
+# input_var = raw_input("Enter single search term or phrase: ")
+# input_var = "'%"+input_var+"%'"   # format SQL string
 
 try:
-  cur.execute("""SELECT DISTINCT ON(created_at) created_at, author FROM hn_comments WHERE comment_text LIKE %s ORDER BY created_at ASC""" % input_var)
+  # cur.execute("""SELECT DISTINCT ON(created_at) created_at, author FROM hn_comments WHERE comment_text LIKE %s ORDER BY created_at ASC""" % input_var)
+  # hardcoded for testing
+  cur.execute("""SELECT DISTINCT ON(created_at) created_at, author FROM hn_comments WHERE comment_text LIKE '%segment.io%' ORDER BY created_at ASC""")
 except:
   print "Could not run select command"
 
 rows = cur.fetchall()
 
-for row in rows:
-  print "r: ", row
+# for row in rows:
+#   print "r: ", row[0]
+dates = tuple(x[0] for x in rows)
 
+ts = pd.Series(dates)
+ts.plot()
+
+plt.show()
+
+
+cur.close()
+conn.close()
